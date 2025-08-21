7d4d22f5b20acf753199da7daab752a9551cb220
Yonder Blue <fields@yonder.blue>
2025-08-21 10:30:35 -0400

    add Locker support (#3)
    This can allow locking for each key, where the key's lock is a
    semaphore.
    
    Benchmark (with profiler commented out):
    ```
    BenchmarkKMutex_uniqueKeys
    BenchmarkKMutex_uniqueKeys/grax_mu
    BenchmarkKMutex_uniqueKeys/grax_mu-8         	      20	 104068229 ns/op
    BenchmarkKMutex_uniqueKeys/grax_sema_2
    BenchmarkKMutex_uniqueKeys/grax_sema_2-8     	      20	 120521967 ns/op
    BenchmarkKMutex_uniqueKeys/immortal
    BenchmarkKMutex_uniqueKeys/immortal-8        	      20	 125882290 ns/op
    PASS
    ```

--- kmutex.go
+++ kmutex.go
@@ -6,13 +6,47 @@ import (
 	"github.com/graxinc/syncmap"
 )
 
+// Must compare equal per instance.
+type Locker interface {
+	Lock()
+	Unlock()
+}
+
+type SemaLocker struct {
+	ch chan struct{}
+}
+
+// n must be > 0.
+func NewSemaLocker(n int) SemaLocker {
+	if n <= 0 {
+		panic("kmutex: sema locker n <= 0")
+	}
+	return SemaLocker{make(chan struct{}, n)}
+}
+
+func (l SemaLocker) Lock() {
+	l.ch <- struct{}{}
+}
+
+func (l SemaLocker) Unlock() {
+	<-l.ch
+}
+
 // Concurrent safe.
 type KMutex[T comparable] struct {
-	m syncmap.Map[T, *sync.Mutex]
+	m         syncmap.Map[T, Locker]
+	newLocker func() Locker
 }
 
 func New[T comparable]() *KMutex[T] {
-	return &KMutex[T]{}
+	l := func() Locker {
+		return &sync.Mutex{}
+	}
+	return NewLocker[T](l)
+}
+
+func NewLocker[T comparable](newLocker func() Locker) *KMutex[T] {
+	return &KMutex[T]{newLocker: newLocker}
 }
 
 // Takes an exclusive lock for key.
@@ -21,9 +55,9 @@ func (km *KMutex[T]) Lock(key T) (unlock func()) {
 	// Some overhead here on the atomic ops in the Map,
 	// but the goal is low contention when most Lock(key)
 	// are with unique keys. Shared keys will contend anyways
-	// on their lock, since that is the point of KMutex.
+	// on their underlying, since that is the point.
 	for {
-		mu, _ := km.m.LoadOrStore(key, &sync.Mutex{})
+		mu, _ := km.m.LoadOrStore(key, km.newLocker())
 		mu.Lock()
 
 		// For !ok case, Delete will happen before this Lock.
